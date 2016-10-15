275bd33cc0d183976eb977a683f776ac7cb63ab5
DESKTOP-IN52JGP\alshd <alshdavid@gmail.com>
2016-10-15 20:23:43 +1300

    login and retrieve messages

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var Conversation = require('./lib/conversation')
 var Accounts = require('./lib/accounts')
 var accounts = new Accounts
+var conversation = new Conversation()
 
 io.on('connection', function(socket){ 
     //on new connection ask for auth
@@ -16,27 +18,38 @@ io.on('connection', function(socket){
     //AUTH ---------
     //on a recieved auth message
     socket.on('auth', function(msg){
-        var userID = accounts.getAccount(msg.username)
-
-        if (userID) {
-            var res = accounts.loginUser(userID, socket.id)
-
-            //errors
-            if (res) {
-                io.to(socket.id).emit( "auth", { action : 'success' } )
-            } else {
-                io.to(socket.id).emit( "auth", { action : 'failure' } )
-            }
-        } else {
-            io.to(socket.id).emit( "auth", { action : 'faliure' } )
-        }
+        accounts.loginUser(msg.username, socket.id).then( (res) => 
+        {
+            console.log(res)
+            io.to(socket.id).emit( "auth", { action : 'success', messages : recentMessages } )  
+        
+        }) .catch ( (res) => 
+        {
+            io.to(socket.id).emit( "auth", { action : 'faliure', message : res.toString() } )
+        
+        })
     })
 
     //MESSAGE -------
     socket.on('msg', function(msg){})
+
+
+    //CHAT -------
+    socket.on('chat', function(msg){})
 });
 
 //accounts.dbTest().then( function(reason){} );
+
+// accounts.loginUser("david", "dsad2324da").then(
+//     (res) => {
+//         console.log(res)
+//     }
+// )
+
+//conversation.create([ "5801bfca42755d047e89e1a6", "5801bfca42755d047e89e1a8" ])
+//conversation.create([ "5801bfca42755d047e89e1a6", "5801bfca42755d047e89e1a9" ])
+
+//accounts.test()
 
 
 
